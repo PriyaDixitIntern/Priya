68b0b357c9d08582b744901432498f6eaf495361
Priya Dixit <96617597+PriyaDixitIntern@users.noreply.github.com>
2022-07-12 23:09:44 +0530

    Update build-assets.js

--- scripts/build-assets.js
+++ scripts/build-assets.js
@@ -2,4 +2,24 @@
 
 const renderAssets = require('./render-assets');
 
-renderAssets();
+rend// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDx8Uhh3i5ZZzzHVfGXpbqyn_S7L5iKSgU",
+  authDomain: "saurabh-9cb43.firebaseapp.com",
+  projectId: "saurabh-9cb43",
+  storageBucket: "saurabh-9cb43.appspot.com",
+  messagingSenderId: "816302718256",
+  appId: "1:816302718256:web:1322a00711e276c5c25cf5",
+  measurementId: "G-GSBK9THKLR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);erAssets();
